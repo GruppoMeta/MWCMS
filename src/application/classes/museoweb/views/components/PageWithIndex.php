d38daf7073ec0bb450e13398df85f78ba6d3e06a
Daniele Ugoletti <daniele.ugoletti@gruppometa.it>
2017-11-03 17:42:45 +0100

    Fix PageWithIndex page

--- src/application/classes/museoweb/views/components/PageWithIndex.php
+++ src/application/classes/museoweb/views/components/PageWithIndex.php
@@ -24,45 +24,36 @@ class museoweb_views_components_PageWithIndex extends org_glizy_components_Repea
 
 	function process()
 	{
-		// check if the component is used in the admin or the the site
-		if (!org_glizy_ObjectValues::get('org.glizy', 'admin', false))
+		parent::process();
+
+		// add the virtual menu in the navigation bar
+		$contentArray = $this->newDataFormat ? $this->_content : org_glizy_helpers_Convert::formEditObjectToStdObject($this->_content);
+		$currentMenu = &$this->_application->getCurrentMenu();
+		$siteMap  = &$this->_application->getSiteMap();
+		
+		for($i=0; $i<$this->numRecords; $i++)
 		{
-
-			$this->numRecords = $this->_parent->loadContent($this->getId());
-
-			// add the virtual menu in the navigation bar
-			$currentMenu 	= &$this->_application->getCurrentMenu();
-			$siteMap		= &$this->_application->getSiteMap();
-			for($i=0; $i<$this->numRecords; $i++)
-			{
-				$suffix = ($i>0) ? '@'.($i) : '';
-				$menu = org_glizy_application_SiteMap::getEmptyMenu();
-				$menu['title'] 		= $this->_parent->loadContent($this->getId().$suffix.'-title');
-				if ( empty( $menu['title'] ) ) continue;
-				$menu['id']			= 9000+$i;
-				$menu['pageType']	= $currentMenu->pageType;
-				$menu['printPdf']	= $currentMenu->printPdf;
-				$menu['hasComment']	= $currentMenu->hasComment;
-				$menu['url'] 		= org_glizy_helpers_Link::addParams(array('pag' => $i));
-				$siteMap->addChildMenu($currentMenu, $menu);
-			}
-
-			// is in the site
-			// load only the selected page
-			$currPage = intval(org_glizy_Request::get('pag', 0))+1;
-			$this->setAttribute('start', $currPage);
-			$this->setAttribute('count', 1);
-
-			if ( $currPage != 1 )
-			{
-				$this->_application->setPageId(9000+$currPage-1);
-				$evt = array('type' => GLZ_EVT_SITEMAP_UPDATE );
-				$this->dispatchEvent($evt);
-			}
-
+			$menu = org_glizy_application_SiteMap::getEmptyMenu();
+			$menu['title'] 		= $contentArray[$i]->title;
+			if ( empty( $menu['title'] ) ) continue;
+			$menu['id']			= 9000+$i;
+			$menu['pageType']	= $currentMenu->pageType;
+			$menu['printPdf']	= $currentMenu->printPdf;
+			$menu['hasComment']	= $currentMenu->hasComment;
+			$menu['url'] 		= org_glizy_helpers_Link::addParams(array('pag' => $i));
+			$siteMap->addChildMenu($currentMenu, $menu);
 		}
 
-		parent::process();
+		// load only the selected page
+		$currPage = intval(org_glizy_Request::get('pag', 0));
+		$this->contentCount = $currPage;
+
+		if ( $this->numRecords > 0 ) {
+			$this->_application->setPageId(9000+$currPage);
+			$evt = array('type' => GLZ_EVT_SITEMAP_UPDATE );
+			$this->dispatchEvent($evt);
+		}
+
+		org_glizy_components_Component::process();
 	}
 }
-?>
